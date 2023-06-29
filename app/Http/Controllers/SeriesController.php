10cc018aa41b1796008eb3e5a1dd15c1436b0a89
Lucas Miotto <latmiotto@gmail.com>
2023-06-29 10:38:38 -0300

    implementando flash messages e atualizar o delete.

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
 
         // retornar a chave e variavel;
         // return view('listar-series', [
@@ -18,25 +19,25 @@ class SeriesController extends Controller
         // return view('listar-series')->with('series', $series);
 
         // ou utilizar o campact.
-        return view('series.index', compact('series'));
+        return view('series.index', compact('series', 'mensagemSucesso'));
     }
 
     public function create()
     {
         return view('series.create');
     }
+
     public function store(Request $request)
     {
-        Serie::create($request->all());
+        $serie = Serie::create($request->all());
 
-        // return redirect()->route('series.index');
-        return to_route('series.index');
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function destroy(Request $request) {
-        Serie::destroy($request->series);
+    public function destroy(Serie $series)
+    {
+        $series->delete();
 
-        return to_route('series.index');
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
     }
-
 }
